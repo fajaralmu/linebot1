8144ff43d1e37e8b83c5ff681ce5429e8bde15cd
Fajar AM <somabangsa@gmail.com>
2021-04-11 16:35:53 +0700

    update config

--- src/main/java/com/fajar/linebot/app/controller/RestController.java
+++ src/main/java/com/fajar/linebot/app/controller/RestController.java
@@ -10,6 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+	@GetMapping("/")
+    public Map<String, Object> index(HttpServletRequest httpServletRequest) {
+        
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		map.put("message", "INDEX ROUTE");
+		map.put("date", new Date());
+		return map;
+    }
 	@GetMapping("hello")
     public Map<String, Object> callback(HttpServletRequest httpServletRequest) {
         
