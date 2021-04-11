e4591ce13760a9c54ade9bf5c629249c7870542f
Fajar AM <somabangsa@gmail.com>
2021-04-11 16:34:09 +0700

    add events handlers

--- src/main/java/com/fajar/linebot/app/EchoApplicationTest.java
+++ src/main/java/com/fajar/linebot/app/EchoApplicationTest.java
@@ -1,42 +1,42 @@
- 
-
-package com.fajar.linebot.app;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
-@SpringBootApplication
-@LineMessageHandler
-public class EchoApplicationTest {
-    private final Logger log = LoggerFactory.getLogger(EchoApplicationTest.class);
-
-    public static void main(String[] args) {
-        SpringApplication.run(EchoApplicationTest.class, args);
-    }
-    
-    public EchoApplicationTest() {
-    	System.out.println(":::::::::::: INITIALIZING EchoApplicationTest");
-    }
-
-    @EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-    	System.out.println("handleTextMessageEvent: " + event);
-        final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
-    }
-
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("handleDefaultMessageEvent: " + event);
-    }
-}
+// 
+//
+//package com.fajar.linebot.app;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//import com.linecorp.bot.model.event.Event;
+//import com.linecorp.bot.model.event.MessageEvent;
+//import com.linecorp.bot.model.event.message.TextMessageContent;
+//import com.linecorp.bot.model.message.Message;
+//import com.linecorp.bot.model.message.TextMessage;
+//import com.linecorp.bot.spring.boot.annotation.EventMapping;
+//import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+//
+//@SpringBootApplication
+//@LineMessageHandler
+//public class EchoApplicationTest {
+//    private final Logger log = LoggerFactory.getLogger(EchoApplicationTest.class);
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(EchoApplicationTest.class, args);
+//    }
+//    
+//    public EchoApplicationTest() {
+//    	System.out.println(":::::::::::: INITIALIZING EchoApplicationTest");
+//    }
+//
+//    @EventMapping
+//    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+//    	System.out.println("handleTextMessageEvent: " + event);
+//        final String originalMessageText = event.getMessage().getText();
+//        return new TextMessage(originalMessageText);
+//    }
+//
+//    @EventMapping
+//    public void handleDefaultMessageEvent(Event event) {
+//        System.out.println("handleDefaultMessageEvent: " + event);
+//    }
+//}
