33378db713861480b13090a99f6acb3aead371b2
Fajar AM <somabangsa@gmail.com>
2021-04-11 16:20:06 +0700

    add controller

--- src/main/java/com/fajar/linebot/app/controller/RestController.java
+++ src/main/java/com/fajar/linebot/app/controller/RestController.java
@@ -1,6 +1,21 @@
 package com.fajar.linebot.app.controller;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-
+	@GetMapping("hello")
+    public Map<String, Object> callback(HttpServletRequest httpServletRequest) {
+        
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		map.put("message", "Hello World");
+		map.put("date", new Date());
+		return map;
+    }
 }
